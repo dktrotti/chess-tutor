f05c21b1870f381b4a64a0ee3c2813207a8b8f38
Dominic Trottier <domtrottier@outlook.com>
2021-12-04 15:51:54 -0800

    Added temp code for parsing PGNs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 import { TutorBoard } from './TutorBoard.js'
+import PgnParser from 'pgn-parser';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <TutorBoard/>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      pgn: null
+    }
+  }
+
+  updateInputValue(evt) {
+    const [pgn] = this.tryParsePgn(evt.target.value)
+    if (pgn !== null) {
+      this.setState({
+        pgn: pgn
+      })
+    }
+  }
+
+  tryParsePgn(pgnStr) {
+    try {
+      return PgnParser.parse(pgnStr)
+    } catch(err) {
+      return null
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>1. e4 e5 2. Nf3 Nc6 3. Bc4 *</p>
+          <input value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)}/>
+          <TutorBoard pgnLine={this.state.pgn}/>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
