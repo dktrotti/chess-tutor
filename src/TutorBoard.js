afe9dd49fe04bf47a607d82733204a048df644f6
Dominic Trottier <domtrottier@outlook.com>
2021-12-04 13:26:10 -0800

    Added basic chessboard

---
+++ src/TutorBoard.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Chessboard } from 'react-chessboard'
+
+export { TutorBoard }
+
+class TutorBoard extends React.Component {
+  render() {
+    return (
+      <div>
+        <Chessboard />
+      </div>
+    )
+  }
+}
