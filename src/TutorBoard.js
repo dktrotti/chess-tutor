b5fe7d1eeb7b7c0caa47b684d7597e2a21eeb2f3
Dominic Trottier <domtrottier@outlook.com>
2021-12-04 14:54:09 -0800

    Board disallows illegal moves

--- src/TutorBoard.js
+++ src/TutorBoard.js
@@ -1,14 +1,37 @@
 import React from "react"
 import { Chessboard } from 'react-chessboard'
-
-export { TutorBoard }
+import Chess from 'chess.js'
 
 class TutorBoard extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { game: new Chess() }
+  }
+
+  updateGameState(updateFunc) {
+    this.setState(prev => {
+      updateFunc(prev.game)
+      return prev
+    })
+  }
+
+  onDrop(sourceSquare, targetSquare) {
+    let move = null
+    this.updateGameState(game => {
+      move = game.move({ from: sourceSquare, to:targetSquare, promotion: 'q' })
+    })
+    // move will be null if the move was illegal
+    return move !== null
+  }
+
   render() {
     return (
       <div>
-        <Chessboard />
+        <Chessboard position={this.state.game.fen()} onPieceDrop={(s, t)=>this.onDrop(s, t)}/>
       </div>
     )
   }
 }
+
+export { TutorBoard }
